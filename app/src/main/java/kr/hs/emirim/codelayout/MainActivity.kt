c77fa9695898ad5eff3f885a36d18281868ac4a9
Kim Hyunji <s2208@e-mirim.hs.kr>
2023-03-28 19:46:28 +0900

    create button / upload link

---
+++ app/src/main/java/kr/hs/emirim/codelayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.hs.emirim.codelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.emirim.testandroid.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
